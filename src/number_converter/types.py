3c289c434b630e9df440b781eeebd86f541a85f8
Sergei Mikurov <454004@mail.ru>
2025-09-29 22:34:41 +0500

    Add library types
    - Added `GenderType`, `CaseType` aliases for function params typing
    - Added `Gender` for gender presentation of number
    - Added `Case` for declension of number by case
    - Added `GENDERS`, `CASES` for mapping aliases of function params
      with attributes of `Gender`, `Case` objects

---
+++ src/number_converter/types.py
@@ -0,0 +1,41 @@
+"""Library types."""
+
+from typing import Literal, NamedTuple
+
+# The function accepts gender and case aliases
+# to convert a number to a word.
+GenderType = Literal['M', 'F', 'N']
+CaseType = Literal['N', 'G', 'D', 'A', 'I', 'P']
+
+GENDERS: dict[GenderType, str] = {
+    'M': 'masculine',
+    'F': 'feminine',
+    'N': 'neuter',
+}
+CASES: dict[CaseType, str] = {
+    'N': 'nominative',
+    'G': 'genitive',
+    'D': 'dative',
+    'A': 'accusative',
+    'I': 'instrumental',
+    'P': 'prepositional',
+}
+
+
+class Gender(NamedTuple):
+    """Gender presentation of number."""
+
+    masculine: str
+    feminine: str
+    neuter: str
+
+
+class Case(NamedTuple):
+    """Declension of number by case."""
+
+    nominative: Gender
+    genitive: Gender
+    dative: Gender
+    accusative: Gender
+    instrumental: Gender
+    prepositional: Gender
