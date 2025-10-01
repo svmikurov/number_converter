8c4b2828abc2c6e13e1c8d2549de9745d3d03f3b
Sergei Mikurov <454004@mail.ru>
2025-10-01 17:56:20 +0500

    Add logging
    - Added logging for case and gender validation

--- src/number_converter/main.py
+++ src/number_converter/main.py
@@ -1,7 +1,11 @@
 """Function for converting an integer to text in words."""
 
+import logging
+
 from . import numbers
 from .types import CASES, GENDERS, Case, CaseType, GenderType
+
+log = logging.getLogger(__name__)
 
 
 class NumberConverter:
@@ -14,16 +18,36 @@ class NumberConverter:
         number_mapping: dict[int, Case] = numbers.prime_numbers,
     ) -> None:
         """Construct the converter."""
-        self._gender = gender
-        self._case = case
+        self._gender = self._set_gender(gender)
+        self._case = self._set_case(case)
         self._number_mapping = number_mapping
 
     def get_text(self, number: int) -> str:
         """Get the text representation of number."""
         cases = self._number_mapping.get(number)
-        genders = getattr(cases, CASES[self._case])
-        word: str = getattr(genders, GENDERS[self._gender])
-        return word
+        genders = getattr(cases, self._case)
+        word = getattr(genders, self._gender)
+        return str(word)
+
+    def _set_gender(self, value: GenderType) -> str:
+        try:
+            return GENDERS[value]
+        except KeyError:
+            log.exception(
+                f"Error number converting, got unexpected '{value}' alias for "
+                f'gender. Use {GenderType} aliases.'
+            )
+            raise
+
+    def _set_case(self, value: CaseType) -> str:
+        try:
+            return CASES[value]
+        except KeyError:
+            log.exception(
+                f"Error number converting, got unexpected '{value}' alias for "
+                f'case. Use {CaseType} aliases.'
+            )
+            raise
 
 
 def convert_number(number: int, gender: GenderType, case: CaseType) -> str:
