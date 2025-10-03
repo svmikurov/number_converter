6047172fa6eb48761a0ecfd75f66c28be1e70557
Sergei Mikurov <454004@mail.ru>
2025-10-03 23:17:52 +0500

    Add ABC for converter of number period to numeral
    - Added `PeriodConvertorABC`

--- src/number_converter/base.py
+++ src/number_converter/base.py
@@ -2,6 +2,8 @@
 
 from abc import ABC, abstractmethod
 
+from .types import CaseType
+
 
 class NumberConverterABC(ABC):
     """The converter of integer to numeral."""
@@ -9,3 +11,11 @@ class NumberConverterABC(ABC):
     @abstractmethod
     def get_text(self, number: int) -> str:
         """Get the text representation of number."""
+
+
+class PeriodConvertorABC(ABC):
+    """The converter of number period to numeral."""
+
+    @abstractmethod
+    def get_text(self, number: int, case: CaseType) -> str:
+        """Get the number period numeral."""
