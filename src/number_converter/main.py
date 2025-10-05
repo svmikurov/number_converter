51fa172d6622905752bcf1a0bbf3169fbbec0919
Sergei Mikurov <454004@mail.ru>
2025-10-05 16:45:23 +0500

    Refactor
    - Fixed typo
    - Renamed variables
    - Simplified logic

--- src/number_converter/main.py
+++ src/number_converter/main.py
@@ -13,7 +13,7 @@ from .types import (
 )
 
 MAX_NUMBER = 999_999_999_999
-LAST_DIGIT_DEVISOR = 10
+LAST_DIGIT_DIVISOR = 10
 
 
 def validate_number(number: int) -> None:
@@ -52,26 +52,26 @@ def get_numeral(
 
     # The hundreds are converted separately.
     if hundreds := number // Factor.HUNDREDS:
-        digits = hundreds * Factor.HUNDREDS
-        numeral = converter.get_text(digits, gender, case)
+        hundreds_part = hundreds * Factor.HUNDREDS
+        numeral = converter.get_text(hundreds_part, gender, case)
         numerals.append(numeral)
 
-    if tens := number // Factor.TENS % LAST_DIGIT_DEVISOR:
+    if tens := number // Factor.TENS % LAST_DIGIT_DIVISOR:
         if tens == 1:
             # The numbers 10, ..., 19 are converted separately.
-            digits = number % Factor.HUNDREDS
-            numeral = converter.get_text(digits, gender, case)
+            tens_part = number % Factor.HUNDREDS
+            numeral = converter.get_text(tens_part, gender, case)
             numerals.append(numeral)
             return ' '.join(numerals)
         else:
             # The tens 20, 30, ..., 90 are converted separately.
-            digits = tens * Factor.TENS
-            numeral = converter.get_text(digits, gender, case)
+            tens_part = tens * Factor.TENS
+            numeral = converter.get_text(tens_part, gender, case)
             numerals.append(numeral)
 
     # The units are converted separately.
-    if digit := number % LAST_DIGIT_DEVISOR:
-        numeral = converter.get_text(digit, gender, case)
+    if units := number % LAST_DIGIT_DIVISOR:
+        numeral = converter.get_text(units, gender, case)
         numerals.append(numeral)
 
     return ' '.join(numerals)
@@ -127,37 +127,35 @@ def convert_number_(
     validate_number(number)
 
     if number == 0:
-        return number_converter.get_text(number, gender, case)
+        return number_converter.get_text(0, gender, case)
 
-    number_ = number
+    remaining = number
     factor_exponent = 0
-    numerals: list[str] = []
+    parts: list[str] = []
 
-    while number_:
+    while remaining:
         # The number is determines by parts
         # that are multiples of a thousand.
-        digits = number_ % Factor.THOUSANDS
-        number_ //= Factor.THOUSANDS
-        factor = Factor(Factor.THOUSANDS**factor_exponent)
+        digits = remaining % Factor.THOUSANDS
+        remaining //= Factor.THOUSANDS
 
-        if not digits:
-            factor_exponent += 1
-            continue
-        else:
-            digits_numeral = get_numeral(
+        if digits > 0:
+            factor = Factor(Factor.THOUSANDS**factor_exponent)
+
+            numeral_part = get_numeral(
                 number_converter,
                 digits,
                 # The factor determines the gender of the multiplicand.
                 gender if factor < Factor.THOUSANDS else factor.gender,
                 case,
             )
-            numerals.insert(0, digits_numeral)
+            parts.insert(0, numeral_part)
 
-        # Factors of a number are converted separately.
-        if factor >= Factor.THOUSANDS:
-            factor_numeral = factor_converter.get_text(digits, case, factor)
-            numerals.insert(1, factor_numeral)
+            # Factors of a number are converted separately.
+            if factor >= Factor.THOUSANDS:
+                factor_text = factor_converter.get_text(digits, case, factor)
+                parts.insert(1, factor_text)
 
         factor_exponent += 1
 
-    return ' '.join(numerals)
+    return ' '.join(parts)
